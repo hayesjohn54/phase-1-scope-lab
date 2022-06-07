447c6bf89530661b9f5d0463bdfa85af00f0bacd
John Hayes <hayesjohn54@yahoo.com>
2022-06-07 18:27:32 -0400

    commit tree 37ad19470e2bd1bab3c04b8057e86174014fc4fb

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+
+function setBestCustomer(){
+    return bestCustomer = "not bob";
+
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "John Hayes";
+
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Ryan Motolla";
+}
